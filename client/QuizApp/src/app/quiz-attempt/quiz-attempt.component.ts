fc1a955cc29fcd072fff64b68066e574662f9483
AjayBisht19 <ajayb381@gmail.com>
2021-03-04 17:55:21 +0530

    added result and review function

--- client/QuizApp/src/app/quiz-attempt/quiz-attempt.component.ts
+++ client/QuizApp/src/app/quiz-attempt/quiz-attempt.component.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute } from '@angular/router';
 import { QuizsService } from './../quizs.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-quiz-attempt',
@@ -11,15 +12,43 @@ export class QuizAttemptComponent implements OnInit {
 
   constructor(private quizService:QuizsService,private router:ActivatedRoute) { }
   questions:{}
+  marks:number;
+  alert:boolean=false;
+  show:boolean=false;
   ngOnInit(): void {
     this.quizService.getquestions(this.router.snapshot.params.id).subscribe((qs)=>{
       this.questions=qs;
-    })
-     
+      console.log(qs)
+    });    
+      this.marks=0;
   }
-
+  answer= new FormGroup({    
+    answer:new FormControl(''), 
+    correct:new FormControl('') 
+  });
+  
   check(){
-    console.log(this.questions)
-  }
+    console.log(this.answer.value['answer']);
+    console.log(this.answer.value['correct']);
+    console.log(this.answer.value)
+    if(this.answer.value['answer'] == this.answer.value['correct']){
+      this.marks++;
+    }
+    }
+
+    submit(){
+      console.log(this.marks);
+      var element = <HTMLInputElement>document.getElementById("solution");
+      element.classList.add("d-none");
+      
+      this.alert=true;
+      
+    }
+    review(){
+      this.show=true;
+      this.alert=false;
+      var element = <HTMLInputElement>document.getElementById("solution");
+      element.classList.remove("d-none");
+    }
 }
 
